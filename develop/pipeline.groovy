979dc623864f08ba2dabf67dc39de155ae6bb610
Wanutpong <frukza3000@hotmail.com>
2025-06-22 18:19:11 +0700

    save

--- develop/pipeline.groovy
+++ develop/pipeline.groovy
@@ -13,14 +13,15 @@ pipeline {
         stage('Test SSH Connection') {
             steps {
                 script {
-                    // ทดสอบ ssh ไปยัง host (ไม่ต้องสั่งคำสั่งอะไร แค่เชื่อมแล้วออก)
-                    def remoteHost = "ssh root@31.97.67.40"
+                    def target = "31.97.67.40"
+                    def status = sh(script: "ping -c 1 -W 2 ${target} > /dev/null 2>&1", returnStatus: true)
 
-                    sh """
-                        echo "Testing SSH to ${remoteHost}..."
-                        ping 31.97.67.40  "echo SSH connection successful"
-                    """
-                    echo 'Connect Success'
+                    if (status == 0) {
+                        echo "✅ Ping to ${target} success"
+                    } else {
+                        echo "❌ Ping to ${target} failed"
+                        error("Ping failed")
+                    }
                 }
             }
         }
