2437fc74b0791ae4b2b91b652ebd70bf6e460427
DESKTOP-UK8MVSK\User <wanutpongbb@gmail.com>
2025-06-24 00:29:54 +0700

    save

--- develop/pipeline.groovy
+++ develop/pipeline.groovy
@@ -24,11 +24,10 @@ pipeline {
                                              usernameVariable: 'SSH_USER')]) {
                     sh """ echo "Connecting to ${env.DEST_IP} as ${SSH_KEY}"  """
                     sh """
-                        ssh -i /root/.ssh/authorized_keys/id_ed25519  -o StrictHostKeyChecking=no ${SSH_USER}@${env.DEST_IP} 'hostname'
+                        ssh -i /home/jenkins/.ssh/id_ed25519  -o StrictHostKeyChecking=no ${SSH_USER}@${env.DEST_IP} 'hostname'
                     """
                 }
             }
         }
     }
 }
-
