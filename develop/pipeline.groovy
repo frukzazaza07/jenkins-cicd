218cc30a1b4e71f11c470ae39d3699878a6bf524
Wanutpong <frukza3000@hotmail.com>
2025-06-24 23:26:50 +0700

    save

--- develop/pipeline.groovy
+++ develop/pipeline.groovy
@@ -31,14 +31,14 @@ pipeline {
                         }
                 }
 
-                withCredentials([sshUserPrivateKey(credentialsId: 'SSH_INSUREOK_SERVER', keyFileVariable: 'SSH_KEY', usernameVariable: 'SSH_USER')]) {
-                    sh """ echo "Connecting to ${env.DEST_IP} as ${SSH_KEY}"  """
-                    sh 'whoami'
-                    sh """ chmod 600 $SSH_KEY  """
-                    sh """
-                        ssh -i ${SSH_KEY} -o StrictHostKeyChecking=no ${SSH_USER}@${env.DEST_IP}
-                    """
-                }
+                // withCredentials([sshUserPrivateKey(credentialsId: 'SSH_INSUREOK_SERVER', keyFileVariable: 'SSH_KEY', usernameVariable: 'SSH_USER')]) {
+                //     sh """ echo "Connecting to ${env.DEST_IP} as ${SSH_KEY}"  """
+                //     sh 'whoami'
+                //     sh """ chmod 600 $SSH_KEY  """
+                //     sh """
+                //         ssh -i ${SSH_KEY} -o StrictHostKeyChecking=no ${SSH_USER}@${env.DEST_IP}
+                //     """
+                // }
             }
         }
     }
