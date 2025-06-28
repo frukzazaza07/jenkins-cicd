7906a6d74599c587ff9dcb613ca706da6f62ba79
DESKTOP-UK8MVSK\User <wanutpongbb@gmail.com>
2025-06-28 22:09:34 +0700

    save

--- develop/pipeline.groovy
+++ develop/pipeline.groovy
@@ -2,26 +2,38 @@ pipeline {
     agent any
     environment {
         DEST_IP = '31.97.67.40'
+        PING_SCRIPT_PATH = './scripts/ping_connection.sh'
+        REPO_NAME = 'insureok'
         GIT_APP_URL = 'https://bitbucket.org/yern/insure-ok.git'
         GIT_APP_BRANCH = 'develop'
         SSH_CREDENTIAL = credentials('SSH_INSUREOK_SERVER')
     }
     stages {
-        stage('Example') {
-            steps {
-                echo 'Hello World'
-            }
-        }
         stage('Test Ping Connection') {
             steps {
-                sh 'chmod +x scripts/ping_connection.sh'
-                sh "./scripts/ping_connection.sh ${env.DEST_IP}"
+                echo "🚀 Starting add chmod script ${env.PING_SCRIPT_PATH}"
+                def resultChmod = sh("chmod +x ${env.PING_SCRIPT_PATH}", returnStatus: true)
+                if(resultChmod == 0){
+                    echo "✅ chmod script success ${env.PING_SCRIPT_PATH}"
+                } else {
+                    echo "❌ chmod script failed ${env.PING_SCRIPT_PATH}"
+                    error("❌ chmod script failed ${env.PING_SCRIPT_PATH}")
+                }
+
+                echo "🚀 Starting ping ${env.PING_SCRIPT_PATH} ${env.DEST_IP}"
+                def resultPing = sh("${env.PING_SCRIPT_PATH} ${env.DEST_IP}", returnStatus: true)
+                if(resultPing == 0){
+                    echo "✅ Ping script success ${env.PING_SCRIPT_PATH} ${env.DEST_IP}"
+                } else {
+                    echo "❌ Ping script failed ${env.PING_SCRIPT_PATH} ${env.DEST_IP}"
+                    error("❌ Ping script failed ${env.PING_SCRIPT_PATH} ${env.DEST_IP}")
+                }
             }
         }
         stage('Test SSH Connection') {
             steps {
                 script {
-                        echo "SSH Connecting to ${env.DEST_IP}"
+                        echo "🚀 Starting SSH Connecting to ${env.DEST_IP}"
                         def status = sshagent(['SSH_INSUREOK_SERVER']) {
                             sh(
                                 script: "ssh -o StrictHostKeyChecking=no ${env.SSH_CREDENTIAL_USR}@${env.DEST_IP} \"hostname -I\"",
@@ -33,7 +45,7 @@ pipeline {
                             echo "✅ SSH test connection success IP: ${env.DEST_IP} User: ${env.SSH_CREDENTIAL_USR}"
                         } else {
                             echo "❌ SSH test connection failed IP: ${env.DEST_IP} User: ${env.SSH_CREDENTIAL_USR} with exit code ${status}"
-                            error("Stop pipeline because SSH failed.")
+                            error("❌ SSH test connection failed IP: ${env.DEST_IP} User: ${env.SSH_CREDENTIAL_USR} with exit code ${status}")
                         }
                 }
             }
@@ -41,12 +53,31 @@ pipeline {
         stage('Pull code app') {
             steps {
                 script {
-                    echo "Started Git pull code from ${env.GIT_APP_URL}"
-                    git(
-                        url: env.GIT_APP_URL,
-                        branch: env.GIT_APP_BRANCH,
-                        credentialsId: 'insureok-bitbucket'
-                    )
+                    try{
+                        echo "🚀 Starting Git pull code from ${env.GIT_APP_URL}"
+                        git(
+                            url: env.GIT_APP_URL,
+                            branch: env.GIT_APP_BRANCH,
+                            credentialsId: 'insureok-bitbucket'
+                        )
+                        echo "✅ Pull code from ${env.GIT_APP_URL} success"
+                    } catch (Exception e) {
+                        echo "❌ Pull code failed: ${e.getMessage()}"
+                        error("❌ Pull code failed: ${e.getMessage()}")
+                    }
+                }
+            }
+        }
+        stage('Build code app') {
+            steps {
+                script {
+                    try{
+                        echo "Starting build code from: ${env.REPO_NAME}"
+                        docker.build(env.REPO_NAME)
+                        echo "✅ Build code from: ${env.REPO_NAME} success"
+                    } catch (Exception e) {
+                        echo "❌ Build code from: ${env.REPO_NAME} failed ${e.getMessage()}"
+                    }
                 }
             }
         }
