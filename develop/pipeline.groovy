c47f20af4f09aca311960a8b6fef73c1dcdde0e5
DESKTOP-UK8MVSK\User <wanutpongbb@gmail.com>
2025-07-01 21:12:09 +0700

    save

--- develop/pipeline.groovy
+++ develop/pipeline.groovy
@@ -90,7 +90,6 @@ pipeline {
                                     script: """
                                         ${env.WORKSPACE}/${env.ENV_SCRIPT_PATH} --url ${env.ENV_SECRET_URL} --path ${env.ENV_SECRET_PATH} --token "\$SECRET_TOKEN"
                                     """,
-                                    returnStatus: true
                                 )
                                     echo "✅ Get ENV success."
                                     echo "$resultGetEnv"
