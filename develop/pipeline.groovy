eee2f37f39f97c6feceef6320506bdc395ab4a6d
DESKTOP-UK8MVSK\User <wanutpongbb@gmail.com>
2025-07-01 19:43:51 +0700

    save

--- develop/pipeline.groovy
+++ develop/pipeline.groovy
@@ -101,8 +101,8 @@ pipeline {
                             echo "$envData.data.APP_NAME"
                             def helper = load 'helpers/helper.groovy'
                             def envContent = helper.jsonToEnv(envData)
-                            writeFile file: '.env', text: envContent
-                            sh(script: "cat .env")
+                            // writeFile file: '.env', text: envContent
+                            // sh(script: "cat .env")
                         }
 
                         echo "✅ Before Build get ENV from: ${env.REPO_NAME} success"
