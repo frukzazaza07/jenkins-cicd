e906fc77d04bc973b19a0991158915957f01b8f1
DESKTOP-UK8MVSK\User <wanutpongbb@gmail.com>
2025-07-01 17:41:30 +0700

    save

--- develop/pipeline.groovy
+++ develop/pipeline.groovy
@@ -85,7 +85,7 @@ pipeline {
                         withCredentials([string(credentialsId: 'ENV_SECRET_AUTH', variable: 'SECRET_TOKEN')]) {
                             def resultGetEnv = sh(
                                 script: """
-                                    ${$WORKSPACE}/${env.ENV_SCRIPT_PATH} --url ${env.ENV_SECRET_URL} --path ${env.ENV_SECRET_PATH} --token "\$SECRET_TOKEN"
+                                    ${env.WORKSPACE}/${env.ENV_SCRIPT_PATH} --url ${env.ENV_SECRET_URL} --path ${env.ENV_SECRET_PATH} --token "\$SECRET_TOKEN"
                                 """,
                                 returnStatus: true
                             )
