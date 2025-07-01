892c97a82cdb9a10aae723c096e6b2a9e12c45ed
DESKTOP-UK8MVSK\User <wanutpongbb@gmail.com>
2025-07-01 17:49:22 +0700

    save

--- develop/pipeline.groovy
+++ develop/pipeline.groovy
@@ -83,6 +83,7 @@ pipeline {
                         echo "Starting get ENV from: ${env.REPO_NAME}"
                         
                         withCredentials([string(credentialsId: 'ENV_SECRET_AUTH', variable: 'SECRET_TOKEN')]) {
+                            sh(script: "chmod +x ${env.WORKSPACE}/${env.ENV_SCRIPT_PATH} ", returnStatus: true)
                             def resultGetEnv = sh(
                                 script: """
                                     ${env.WORKSPACE}/${env.ENV_SCRIPT_PATH} --url ${env.ENV_SECRET_URL} --path ${env.ENV_SECRET_PATH} --token "\$SECRET_TOKEN"
