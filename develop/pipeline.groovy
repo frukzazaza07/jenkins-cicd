c6c83e0d7e9ccda9655034cf2bf9ffd40371bb9b
DESKTOP-UK8MVSK\User <wanutpongbb@gmail.com>
2025-07-01 20:31:52 +0700

    save

--- develop/pipeline.groovy
+++ develop/pipeline.groovy
@@ -97,10 +97,10 @@ pipeline {
                                     def jsonSlurper = new groovy.json.JsonSlurper()
                                     def envData = jsonSlurper.parseText(resultGetEnv)
                                     def jsonStringForEnv = JsonOutput.toJson(envData.data)
-                                    def resultCreateEnv = sh(script: """${env.WORKSPACE}/${env.CONVERT_JSON_TO_ENV_SCRIPT_PATH} '$jsonStringForEnv' """, returnStatus: true)
-                                    if(resultCreateEnv == 0){
-                                        echo "GGG"
-                                    }
+                                    // def resultCreateEnv = sh(script: """${env.WORKSPACE}/${env.CONVERT_JSON_TO_ENV_SCRIPT_PATH} '$jsonStringForEnv' """, returnStatus: true)
+                                    // if(resultCreateEnv == 0){
+                                    //     echo "GGG"
+                                    // }
                             }
                         }
 
