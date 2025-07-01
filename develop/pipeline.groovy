43786b0823e97c1931fbb245fe54a889526d179a
DESKTOP-UK8MVSK\User <wanutpongbb@gmail.com>
2025-07-01 18:59:05 +0700

    save

--- develop/pipeline.groovy
+++ develop/pipeline.groovy
@@ -89,7 +89,7 @@ pipeline {
                                 """,
                                 returnStdout: true
                             )
-                            echo "$resultGetEnv"
+                            echo "$resultGetEnv.status"
                             if(resultGetEnv == 0){
                                 def fullJsonResponse = scriptResult.stdout.trim()
                                 echo "✅ Before Build script success."
