4c5def34e79fc2797fd8b10c58325478e939f17b
DESKTOP-UK8MVSK\User <wanutpongbb@gmail.com>
2025-07-01 21:49:10 +0700

    save

--- scripts/get_env.sh
+++ scripts/get_env.sh
@@ -80,7 +80,7 @@ fi
 
 echo "$responseBody" | jq .data
 # echo "$responseBody" | jq '.data -r | to_entries[] | "\(.key)=\(.value)"' > .env
-echo "$responseBody" | jq .data -r 'to_entries[] | "\(.key)=\(.value)"' > .env
+echo "$responseBody" | jq -r '.data | to_entries | .[] | .key + "=" + (.value | @sh)'
 
 
 echo 'GG'
