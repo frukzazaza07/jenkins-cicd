9bbbdca6bcef38da8d2182a8d8e7e762b7585199
DESKTOP-UK8MVSK\User <wanutpongbb@gmail.com>
2025-07-01 20:25:04 +0700

    save

--- develop/pipeline.groovy
+++ develop/pipeline.groovy
@@ -84,24 +84,23 @@ pipeline {
                 script {
                     try{
                         echo "Starting get ENV from: ${env.REPO_NAME}"
-                        withCredentials([string(credentialsId: 'ENV_SECRET_AUTH', variable: 'SECRET_TOKEN')]) {
-                            def resultGetEnv = sh(
-                                script: """
-                                    ${env.WORKSPACE}/${env.ENV_SCRIPT_PATH} --url ${env.ENV_SECRET_URL} --path ${env.ENV_SECRET_PATH} --token "\$SECRET_TOKEN"
-                                """,
-                                returnStdout: true
-                            )
-                            dir('application'){
-                                echo "✅ Get ENV success."
-                                echo "🚀 Process parse json"
-                                
-                                def jsonSlurper = new groovy.json.JsonSlurper()
-                                def envData = jsonSlurper.parseText(resultGetEnv)
-                                def jsonStringForEnv = JsonOutput.toJson(envData.data)
-                                def resultCreateEnv = sh(script: "${env.CONVERT_JSON_TO_ENV_SCRIPT_PATH} '$jsonStringForEnv' ", returnStatus: true)
-                                if(resultCreateEnv == 0){
-                                    echo "GGG"
-                                }
+                        dir('application'){
+                            withCredentials([string(credentialsId: 'ENV_SECRET_AUTH', variable: 'SECRET_TOKEN')]) {
+                                def resultGetEnv = sh(
+                                    script: """
+                                        ${env.WORKSPACE}/${env.ENV_SCRIPT_PATH} --url ${env.ENV_SECRET_URL} --path ${env.ENV_SECRET_PATH} --token "\$SECRET_TOKEN"
+                                    """,
+                                    returnStdout: true
+                                )
+                                    echo "✅ Get ENV success."
+                                    echo "🚀 Process parse json"
+                                    def jsonSlurper = new groovy.json.JsonSlurper()
+                                    def envData = jsonSlurper.parseText(resultGetEnv)
+                                    def jsonStringForEnv = JsonOutput.toJson(envData.data)
+                                    def resultCreateEnv = sh(script: "${env.CONVERT_JSON_TO_ENV_SCRIPT_PATH} '$jsonStringForEnv' ", returnStatus: true)
+                                    if(resultCreateEnv == 0){
+                                        echo "GGG"
+                                    }
                             }
                         }
 
