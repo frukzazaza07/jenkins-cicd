1d92cca0d25bfdb634f8b069c71f3396b07378f8
DESKTOP-UK8MVSK\User <wanutpongbb@gmail.com>
2025-07-10 22:12:02 +0700

    save

--- vars/initDefaultParams.groovy
+++ vars/initDefaultParams.groovy
@@ -1,5 +1,6 @@
 def getDefaultParams() {
     return [
+        choice(name: 'Repository', choices: ['fe-fo', 'fe-bo', 'b-api', 's-api', 'new-homepage'], description: 'Choose environment'),
         string(name: 'Branch', defaultValue: 'develop', description: 'Enter your branch (required)'),
         choice(name: 'ENV', choices: ['develop', 'staging'], description: 'Choose environment'),
         string(name: 'AppVersion', defaultValue: '', description: 'Enter your app version (required)'),
