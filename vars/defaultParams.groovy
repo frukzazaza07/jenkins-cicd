06e65481f83c609349db91ef38d77f733f510748
DESKTOP-UK8MVSK\User <wanutpongbb@gmail.com>
2025-07-10 19:42:42 +0700

    save

--- vars/defaultParams.groovy
+++ vars/defaultParams.groovy
@@ -2,6 +2,9 @@ def call() {
     println "[DEBUG] pipelineParams.groovy is loaded"
     echo "zzzzzzzzzzzzzzzzz"
     return {
+         script {
+            echo "[DEBUG] pipelineParams closure invoked ✅"
+        }
         string(name: 'Branch', defaultValue: 'develop', description: 'Enter your branch (required)')
         choice(name: 'ENV', choices: ['develop', 'staging'], description: 'Choose environment')
         string(name: 'AppVersion', defaultValue: 'develop', description: 'Enter your app version (required)')
