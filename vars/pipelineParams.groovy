a5082326fb3e5b0595e9053cec44710c16645853
DESKTOP-UK8MVSK\User <wanutpongbb@gmail.com>
2025-07-10 19:08:01 +0700

    save

--- vars/pipelineParams.groovy
+++ vars/pipelineParams.groovy
@@ -1,7 +1,9 @@
 def call() {
     // generate params build with params
     // config on jenkins website manage jenkins -> config system -> find Global Trusted Pipeline Libraries
-    string(name: 'Branch', defaultValue: 'develop', description: 'Enter your branch (required)')
-    choice(name: 'ENV', choices: ['develop', 'staging'], description: 'Choose environment')
-    string(name: 'App Version', defaultValue: '', description: 'Enter your app version (required)')
+    return {
+        string(name: 'Branch', defaultValue: 'develop', description: 'Enter your branch (required)'),
+        choice(name: 'ENV', choices: ['develop', 'staging'], description: 'Choose environment'),
+        string(name: 'App Version', defaultValue: '', description: 'Enter your app version (required)')
+    }
 }
