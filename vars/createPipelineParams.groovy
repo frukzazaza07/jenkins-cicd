79d8162073a5eb08d0fb168e85d21740271500df
DESKTOP-UK8MVSK\User <wanutpongbb@gmail.com>
2025-07-10 19:21:48 +0700

    save

--- vars/createPipelineParams.groovy
+++ vars/createPipelineParams.groovy
@@ -1,22 +1,29 @@
-def call() {
-    return [
-        [
-            $class: 'StringParameterDefinition',
-            name: 'Branch',
-            defaultValue: 'develop',
-            description: 'Enter your branch (required)'
-        ],
-        [
-            $class: 'ChoiceParameterDefinition',
-            name: 'ENV',
-            choices: ['develop', 'staging'],
-            description: 'Choose environment'
-        ],
-        [
-            $class: 'StringParameterDefinition',
-            name: 'AppVersion',
-            defaultValue: '',
-            description: 'Enter your app version (required)'
-        ]
-    ]
+def call(List parameterDefinitions): [booleanParam, choice, credentials, file, text, password, run, string] {
+    parameterDefinitions.each { paramDef ->
+        switch (paramDef.$class) {
+            case 'StringParameterDefinition':
+                string(paramDef)
+                break
+            case 'ChoiceParameterDefinition':
+                choice(paramDef)
+                break
+            case 'BooleanParameterDefinition':
+                booleanParam(paramDef)
+                break
+            case 'TextParameterDefinition':
+                text(paramDef)
+                break
+            case 'PasswordParameterDefinition':
+                password(paramDef)
+                break
+            case 'FileParameterDefinition':
+                file(paramDef)
+                break
+            case 'CredentialsParameterDefinition':
+                credentials(paramDef)
+                break
+            default:
+                error "Unsupported parameter type in Shared Library: ${paramDef.$class}"
+        }
+    }
 }
--- vars/pipelineParams.groovy
+++ vars/pipelineParams.groovy
@@ -1,29 +1,23 @@
-def call(List parameterDefinitions): [booleanParam, choice, credentials, file, text, password, run, string] {
-    parameterDefinitions.each { paramDef ->
-        switch (paramDef.$class) {
-            case 'StringParameterDefinition':
-                string(paramDef)
-                break
-            case 'ChoiceParameterDefinition':
-                choice(paramDef)
-                break
-            case 'BooleanParameterDefinition':
-                booleanParam(paramDef)
-                break
-            case 'TextParameterDefinition':
-                text(paramDef)
-                break
-            case 'PasswordParameterDefinition':
-                password(paramDef)
-                break
-            case 'FileParameterDefinition':
-                file(paramDef)
-                break
-            case 'CredentialsParameterDefinition':
-                credentials(paramDef)
-                break
-            default:
-                error "Unsupported parameter type in Shared Library: ${paramDef.$class}"
-        }
-    }
+
+def call() {
+    return [
+        [
+            $class: 'StringParameterDefinition',
+            name: 'Branch',
+            defaultValue: 'develop',
+            description: 'Enter your branch (required)'
+        ],
+        [
+            $class: 'ChoiceParameterDefinition',
+            name: 'ENV',
+            choices: ['develop', 'staging'],
+            description: 'Choose environment'
+        ],
+        [
+            $class: 'StringParameterDefinition',
+            name: 'AppVersion',
+            defaultValue: '',
+            description: 'Enter your app version (required)'
+        ]
+    ]
 }
