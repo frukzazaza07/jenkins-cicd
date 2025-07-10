b6a5ddb92b20e8ce59c96a46b1b0fc2373bded2b
DESKTOP-UK8MVSK\User <wanutpongbb@gmail.com>
2025-07-10 18:55:47 +0700

    save

--- src/jobs/docker-container/develop/pipeline.groovy
+++ src/jobs/docker-container/develop/pipeline.groovy
@@ -1,8 +1,8 @@
 @Library('default-parameters-shared-library') _
 
-// def myParams = {
-//     string(name: 'FOO', defaultValue: 'bar', description: 'Enter something')
-// }
+def myParams = {
+    string(name: 'FOO', defaultValue: 'bar', description: 'Enter something')
+}
 pipeline {
     agent any
 
